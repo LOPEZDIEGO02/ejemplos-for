ce38b19305fefeab541943be08a8da4c67f8309c
LOPEZDIEGO02 <lopezroque0209@gmaail.com>
2025-06-25 01:46:57 -0600

    Ejeplos del uso del FOR

--- src/pedir-numeros.js
+++ src/pedir-numeros.js
@@ -1,2 +1,18 @@
 const prompt = require('prompt-sync')();
 
+let numero;
+
+while (true) { 
+    numero = prompt("Dame varios numeros: "); 
+    
+    
+    if (numero === '0') { 
+        console.log("El programa termina");
+        break; 
+    }
+
+    if(isNaN(numero)){
+        console.log("oyes que crees no es un numero")
+    }
+
+}   
